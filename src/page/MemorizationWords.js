545267193ea7e8f7e333a7af2a1821ae2ea15b83
pcj-leha <99689620+pcj-leha@users.noreply.github.com>
2023-04-09 22:47:37 +0300

    Delete src directory

--- src/page/MemorizationWords.js
+++
@@ -1,8 +0,0 @@
-import { observer } from 'mobx-react-lite'
-import React from 'react'
-
-function MemorizationWords() {
-    return <div>MemorizationWordss</div>
-}
-
-export default observer(MemorizationWords)
